6b04146e348f426ba3b137555d4cdeebd5deb149
puneetmathur25 <puneetmathur25@gmail.com>
2015-10-12 15:21:20 -0400

    added project

---
+++ code/ready-ios-widgets/ready-ios-widgetsTests/ready_ios_widgetsTests.swift
@@ -0,0 +1,36 @@
+//
+//  ready_ios_widgetsTests.swift
+//  ready-ios-widgetsTests
+//
+//  Created by Puneet Mathur on 10/12/15.
+//  Copyright © 2015 Puneet Mathur. All rights reserved.
+//
+
+import XCTest
+@testable import ready_ios_widgets
+
+class ready_ios_widgetsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
